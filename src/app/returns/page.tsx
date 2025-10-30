a25519951d847fcde4404bb0110220e5508316c8
Raheem Orekoya <tundeorekoya1@gmail.com>
2025-10-30 16:08:34 +0100

    feat: add returns page

---
+++ src/app/returns/page.tsx
@@ -0,0 +1,9 @@
+import Returns from "@/markdown/returns.mdx"
+
+export default function Page() {
+   return (
+      <div className="w-[min(1200px,90%)] min-h-screen py-20 mx-auto prose prose-lime prose-p:text-balance">
+         <Returns />
+      </div>
+   );
+}
---
+++ src/app/shipping/page.tsx
@@ -0,0 +1,9 @@
+import Shipping from "@/markdown/shipping.mdx";
+
+export default function Page() {
+   return (
+      <div className="w-[min(1200px,90%)] min-h-screen py-20 mx-auto prose prose-lime prose-p:text-balance">
+         <Shipping />
+      </div>
+   );
+}
